65845ec0e091e0cd1090cb13b17075d22cdab8fa
Gabriel Brookman <gabriel@eduroam-165-193.dyn.carleton.edu>
2020-01-14 15:54:49 -0600

    works, slowly and maybe with bugs

--- watershed.py
+++ watershed.py
@@ -7,21 +7,26 @@ def naive_watershed(image):
     width, height = imageGrey.size
     clumps = []
     accountedFor = []
+    print("starting clumping")
+    #'''
     for x in range(width):
+        print(x)
         for y in range(height):
             if (x,y) in accountedFor:
                 continue
-            newclump = {"pixels":[(x,y)],"borders":[],"value":imageGrey.getPixel((x,y))}
-            print(imageGrey.getPixel((x,y)))
+            newclump = {"pixels":[(x,y)],"borders":[],"value":imageGrey.getpixel((x,y))}
+            #print("START:")
+            #print((x,y))
             maybes = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
             accountedFor.append((x,y))
             while len(maybes) > 0:
                 cand = maybes.pop()
-                if (cand[0] < 0) or (cand[0] == x) or (cand[1] < 0) or (cand[1] == y):
+                #print(cand)
+                if (cand[0] < 0) or (cand[0] == width) or (cand[1] < 0) or (cand[1] == height):
                     continue
                 if cand in newclump["pixels"]:
                     continue
-                if imageGrey.getPixel(cand) == newclump["value"]:
+                if imageGrey.getpixel(cand) == newclump["value"]:
                     newclump["pixels"].append(cand)
                     maybes.append((cand[0]+1,cand[1]))
                     maybes.append((cand[0]-1,cand[1]))
@@ -31,26 +36,35 @@ def naive_watershed(image):
                 else:
                     newclump["borders"].append(cand)
             clumps.append(newclump)
-    pools = []
-    walls = []
+
+    print("done clumping")
+    #print(clumps)
+    pools = []#'''
+    returnImage = np.zeros(shape=(height,width))
+    #'''
     for val in range(256):
+        print(val)
         for clump in clumps:
-            if clump["value"] == val
+            if clump["value"] == val:
                 borderpools = []
                 for pool in pools:
-                    for border in pool["boarders"]:
+                    for border in pool["borders"]:
                         if border in clump["pixels"]:
                             borderpools.append(pool)
                 if len(borderpools) == 0:
-                    #make new pool
+                    newpool = {"borders":clump["borders"]}
+                    pools.append(newpool)
                 elif len(borderpools) == 1:
-                    #add to pool
+                    pool = borderpools[0]
+                    pool["borders"] = [x for x in pool["borders"]+clump["borders"] if x not in clump["pixels"]]
                 else:
-                    for pixel in clump["pixels"]:
-                        walls.append(pixel)
-    #make new image and make the walls black and other stuff white
+                    for (x,y) in clump["pixels"]:
+                        returnImage[y,x] = 255
+    
+    print("done segmenting")
+    return returnImage
 
 if __name__ == "__main__":
     img = Image.open("22093.jpg")
-    img = Image.fromarray(baseline_thresholding(img)*200)
+    img = Image.fromarray(naive_watershed(img))
     img.show()
