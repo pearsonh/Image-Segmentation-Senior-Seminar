19d791c3f7931bc13923d8d7baff81468438543a
pearsonh <henryopearson@gmail.com>
2020-01-21 15:05:52 -0600

    split method complete, working on merge

--- regionsplit.py
+++ regionsplit.py
@@ -1,24 +1,27 @@
 from PIL import Image
 import numpy as np
 import statistics as stat
+import math
 
 def regionSplit(image, threshold):
     imageGrey = image.convert(mode='L')
     imgArray = np.array(imageGrey).astype('int')
-    splitArray = np.array(split(imgArray, threshold))
-    print(splitArray.shape)
-    print(splitArray[0])
-    return merge(splitArray, threshold)
+    splitGraph = np.array(split(imgArray, threshold))
+    return merge(splitGraph, threshold)
 
 def split(img, threshold):
     if flatTest(img, threshold):
         return img
     else:
-        return (split(img[:len(img)//2, 0:len(img)//2], threshold), split(img[:len(img)//2, 0:len(img)//2], threshold),  split(img[len(img)//2:, 0:len(img)//2], threshold), split(img[len(img)//2:, len(img)//2:], threshold))
+        splitNumpy = []
+        splitNumpy.append(split(img[:len(img)//2, :len(img)//2], threshold))
+        splitNumpy.append(split(img[:len(img)//2, len(img)//2:], threshold))
+        splitNumpy.append(split(img[len(img)//2:, :len(img)//2], threshold))
+        splitNumpy.append(split(img[len(img)//2:, len(img)//2:], threshold))
+        return splitNumpy
 
-def merge(img, threshold):
-
-    return
+def merge(splitGraph, threshold):
+    return splitGraph
 
 def flatTest(array, threshold):
     x, y = len(array), len(array[0])
@@ -26,7 +29,7 @@ def flatTest(array, threshold):
     for j in range(x):
         for k in range(y):
             mean.append(array[j,k])
-    if len(mean) <= 4 or stat.variance(mean, None) <= threshold:
+    if len(mean) <= 4 or math.sqrt(stat.variance(mean, None)) <= threshold:
         return True
     else:
         return False
@@ -34,4 +37,4 @@ def flatTest(array, threshold):
 
 if __name__ == '__main__':
     img = Image.open("22093.jpg")
-    new_img = regionSplit(img, 5000)
+    print(regionSplit(img, 50)[0])
