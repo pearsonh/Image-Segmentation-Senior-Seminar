766b6df94fc096adb18c64f54f85e811e9431322
jbaskauf <jessie.baskauf@gmail.com>
2020-02-04 14:34:36 -0600

    Reorganized functions in file

--- mincut.py
+++ mincut.py
@@ -3,26 +3,24 @@ from PIL import Image
 from scipy import *
 from scipy.sparse import *
 from scipy.sparse.linalg import *
-np.set_printoptions(threshold=np.inf)
 
 # compute difference between two pixels in the np array of rgb tuples by euclidean
-#distance (or replace with some other metric in this method)
+# distance between rgb values (or replace with some other metric in this method)
 def differenceMetric(coords1, coords2, pixels):
     rgb1 = pixels[coords1[0], coords1[1]]
     rgb2 = pixels[coords2[0], coords2[1]]
     return ((rgb1[0] - rgb2[0])**2 + (rgb1[1] - rgb2[1])**2 + (rgb1[2] - rgb2[2])**2)
 
-#D is the diagonal matrix and W is the adjacency matrix of edge weights
-#returns a tuple with an array of eigenvalues and an array of eigenvectors
+# D is the diagonal matrix and W is the adjacency matrix of edge weights (both csc sparse matrices)
+# uses scipy sparse linalg eigenvector solver for the Lanczos method
+# returns a tuple containing an array of eigenvalues and an array of eigenvectors
 def findEigens(D, W):
-    #print("Inverse of D", inv(D)) #scipy sparse linalg inverse function
-    #print("D - W:", D - W)
-    return eigsh(inv(D)*(D - W)) #scipy sparse linalg eigenvector solver for Lanczos method (that they used in the paper)
+    return eigsh(inv(D)*(D - W))
 
-#takes in an np array of pixels and returns a sparse adjacency matrix (csc_matrix) with edge weights for this image
+# takes in an np array of pixels and returns a sparse adjacency matrix (csc_matrix) with edge weights for this image
 def pixelsToAdjMatrix(pixels):
-    r = 18
-    y,x,_ = pixels.shape #assuming tuples of 3 values are the third coordinate of the shape
+    r = 10
+    y,x,_ = pixels.shape #assuming tuples of 3 rgb values are the third coordinate of the shape
     N = x * y
     row = []
     col = []
@@ -35,8 +33,6 @@ def pixelsToAdjMatrix(pixels):
                 for l in range(j-r,j+r): # y coordinate of offset pixel
                     if k >= 0 and l >= 0 and k < x and l < y: # make sure this pixel isn't out of bounds
                         diff = differenceMetric((j,i), (l,k), pixels)
-                        #if (diff < 20000): # some threshold, probably dont need this once im using a distance threshold
-                            #matrix[l*x + k, j*x + i] = diff
                         row.append(j*x + i) #add x coord to list of x coords
                         col.append(l*x + k) #add y coord to list of y coords
                         data.append(diff) #add the value that belongs at (j*x + i, l*x + k) in the adjacency matrix
@@ -49,46 +45,30 @@ def adjMatrixToDiagMatrix(matrix):
     #with an array inside), and I can't' figure out how to access just the first row of it
     #other than by changing it to an np array first
     vec = np.array(matrix.sum(axis=0))[0]
+    # change any 0s on the diagonal to 1s (otherwise you can get a singular matrix) - should this be a different value than 1?
     vec = np.where(vec != 0, vec, 1)
     return diags(vec, offsets=0).tocsc()
 
-# simple test, doesn't want to find eigens bc the matrix isn't big enough
-#pixels = np.array([[(0,0,0),(255,255,255)],[(3,3,3), (1,1,1)]])
-# test with actual image
-img = Image.open("test4.jpg")
-pixels = np.array(img).astype("int")
+# takes in an image to segment by mincut, returns an np array of the segment numbers
+# for each pixel
+def mincut(img):
+    pixels = np.array(img).astype("int")
+    y,x,_ = pixels.shape
+    W = pixelsToAdjMatrix(pixels)
+    D = adjMatrixToDiagMatrix(W)
+    eigenStuff = findEigens(D, W)
+    # pick second smallest eigenvector (second column of the array of eigenvectors) - returns as an ndarray
+    eigVec = eigenStuff[1][:, 1]
+    # converts eigvec to an indicator vector with 1 if the value is > 0 and 0 otherwise
+    eigVecIndicator = (eigVec > 0).astype(int)
+    # reshape the indicator vector into a matrix the same shape as pixels
+    newEigIndicator = np.reshape(eigVecIndicator, (y,x))
+    # for some reason the image only displays correctly if you make sure the type is uint8
+    newEigIndicator = (newEigIndicator).astype('uint8')
+    return newEigIndicator
 
-#print("Image:\n", pixels)
-W = pixelsToAdjMatrix(pixels)
-#print("adjacency matrix:\n", W)
-D = adjMatrixToDiagMatrix(W)
-#print("diagonal matrix:\n", D)
-eigenStuff = findEigens(D, W)
-#print(eigenStuff)
-eigVec = eigenStuff[1][:, 1] # pick second smallest eigenvector (second column of the array of eigenvectors) - returns as an ndarray
-#print(eigVec)
-eigVecIndicator = (eigVec > 0).astype(int) # converts eigvec to an indicator vector with 1 if the value is > 0 and 0 otherwise
-#print(eigVecIndicator)
-
-grayImage = img.convert(mode="L")
-grayPixels = np.array(grayImage).astype("int")
-#print(grayPixels)
-y,x = grayPixels.shape
-y,x,_ = pixels.shape
-newEigIndicator = np.reshape(eigVecIndicator, (y,x)) # reshape the indicator vector into a matrix the same shape as graypixels
-# print(newEigIndicator[:, :36])
-# print(newEigIndicator[:, 36:])
-
-newPixels = (grayPixels * newEigIndicator).astype('uint8')
-# for some reason the image only displays correctly if you make sure the type is uint8
-newEigIndicator = (newEigIndicator * 255).astype('uint8')
-#print(newEigIndicator)
-# print(newEigIndicator[:, :36])
-# print(newEigIndicator[:, 36:])
-#print(newPixels)
-img = Image.fromarray(newEigIndicator, mode="L")
-img.show()
-img.save("test4-18-1s.jpg", "JPEG")
-#
-# img = Image.fromarray(newPixels, mode="L")
-# img.show()
+if __name__ == "__main__":
+    img = Image.open("test.png")
+    img = Image.fromarray(mincut(img)*255, mode="L")
+    img.show()
+    img.save("test-10-1s.jpg", "JPEG")
