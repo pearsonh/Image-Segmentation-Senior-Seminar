b58865a3285447fbaeb8db1f8afcf252ae3d1729
Gabriel Brookman <gabriel@eduroam-165-88.dyn.carleton.edu>
2020-02-14 20:40:27 -0600

    integrated blurring. separated naive from pruned

--- watershed.py
+++ watershed.py
@@ -4,7 +4,73 @@ import numpy as np
 from collections import deque
 from random import randrange
 
-def naive_watershed(image,depththreshold):
+def naive_watershed(image,depththreshold,blur=None):
+    #blurs input image
+    if blur:
+        image = image.filter(ImageFilter.GaussianBlur(blur)) #blur
+    #Creates a greyscale version of the input image
+    imageGrey = image.convert(mode="L")
+    imageGrey = np.array(imageGrey.getdata()).reshape(imageGrey.size[1],imageGrey.size[0])
+    #imageGrey = imageGrey - imageGrey%18 #round
+    height, width = imageGrey.shape
+    getNeighbors = lambda x,y: [i for i in [(x,y+1),(x,y-1),(x+1,y),(x-1,y)] if (i[0] >= 0 and i[1] >= 0 and i[0] < height and i[1] < width)]
+    curLabel = 2 # 0 is unlabelled. 1 is WALL
+    pix = [deque() for i in range(256+5)]
+    for x in range(height):
+        for y in range(width):
+            pix[int(imageGrey[(x,y)])].append((x,y))
+    labelImage = np.zeros(shape=(height,width))
+    returnImage = np.zeros(shape=(height,width,3))
+    listofmins = ["???","???"]
+    for val in range(256):
+        while True:
+            try:
+                nextpix = pix[val].pop()
+            except IndexError:
+                break
+            if (labelImage[nextpix] != 0):
+                continue
+            pixList, neighbors = getClumpAndLowNeighbors(imageGrey,nextpix,getNeighbors)
+            labels = []
+            for neighbor in neighbors:
+                if labelImage[neighbor] != 1: #not one of those WALLs
+                    labels.append(labelImage[neighbor])
+                    if labelImage[neighbor] == 0:
+                        raise Exception
+            if len(labels) == 0:
+                label = curLabel
+                curLabel = curLabel + 1
+            else:
+                label = labels[0]
+                for curLab in labels:
+                    if curLab != label:
+                        label = 1
+                        break
+            for pixel in pixList:
+                labelImage[pixel] = label
+
+
+    for x in range(height):
+        for y in range(width):
+            pixel = (x,y)
+            if labelImage[pixel] == 1:
+                pixelsToDye, val = getClumpForDyeing(imageGrey,pixel,getNeighbors,labelImage)
+                for pixel in pixelsToDye:
+                    labelImage[pixel] = val
+
+    colorkey = np.zeros((curLabel,3))
+    for i in range(curLabel):
+        colorkey[i] = (randrange(256),randrange(256),randrange(256))
+    for x in range(height):
+        for y in range(width):
+            pixel = (x,y)
+            returnImage[x,y] = colorkey[int(labelImage[pixel])]
+    return returnImage
+
+def watershed_with_wolf(image,depththreshold,blur=None):
+    #blurs input image
+    if blur:
+        image = image.filter(ImageFilter.GaussianBlur(blur)) #blur
     #Creates a greyscale version of the input image
     imageGrey = image.convert(mode="L")
     imageGrey = np.array(imageGrey.getdata()).reshape(imageGrey.size[1],imageGrey.size[0])
@@ -56,7 +122,6 @@ def naive_watershed(image,depththreshold):
                 for neighbor in labels:
                     if neighbor == 0:
                         continue
-                    print(nextpixval, listofmins[int(neighbor)])
                     if (nextpixval - listofmins[int(neighbor)] > depththreshold):
                         deepEnough.append(neighbor)
                     else:
@@ -127,9 +192,5 @@ def getClumpAndLowNeighbors(img,pix,neighb):
 
 if __name__ == "__main__":
     img = Image.open("22093.jpg")
-    #img = img.filter(ImageFilter.GaussianBlur(2)) #blur
-    newarr = np.zeros((2,2,3))
-    newarr[1,1,:] = [150,150,150]
-    newarr = newarr.astype('uint8')
-    img = Image.fromarray(naive_watershed(img,10).astype('uint8'))
+    img = Image.fromarray(naive_watershed(img,30).astype('uint8'))
     img.show()
