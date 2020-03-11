0cf51e4b85a37bcbd38d83e88facc09ae5571678
pearsonh <henryopearson@gmail.com>
2020-03-10 21:57:34 -0500

    final edits to regionsplit.py

--- regionsplit.py
+++ regionsplit.py
@@ -1,9 +1,6 @@
 from PIL import Image
 import numpy as np
 import statistics as stat
-import math
-import sys
-from eval import region_based_eval
 
 def regionSplitAndMerge(image, threshold):
     '''Base function for split and merge.
@@ -15,7 +12,6 @@ def regionSplitAndMerge(image, threshold):
     img = np.array(imageGrey).astype("int")
     blank = np.empty(img.shape)
     splitMergeSeg = splitAndMerge(img, blank, threshold)
-    #splitSeg = split(img, np.empty(img.shape), threshold)
     return splitMergeSeg
 
 def splitAndMerge(img, blank, threshold):
@@ -24,7 +20,7 @@ def splitAndMerge(img, blank, threshold):
         Recursively tests quadrants of the image to test if they are homogenous regions.
         If not, the regions are split and the test is run on each. If they are, then
         the regions are painted an average color.
-        
+
         As they recurse back up the stack, collections of two or three adjacent quadrants are
         repainted to fix any oversegmenting done in the split phase.'''
     tL = img[:img.shape[0]//2, :img.shape[1]//2]
@@ -32,7 +28,6 @@ def splitAndMerge(img, blank, threshold):
     bL = img[img.shape[0]//2:, :img.shape[1]//2]
     bR = img[img.shape[0]//2:, img.shape[1]//2:]
     if flatTest(tL, threshold) and flatTest(tR, threshold) and flatTest(bL, threshold) and flatTest(bR, threshold):
-        #if flatTest(img, threshold):
         x,y = img.shape
         avgVal = np.mean(img)
         for i in range(x):
@@ -45,6 +40,7 @@ def splitAndMerge(img, blank, threshold):
         splitAndMerge(tR, blank[:bWidth//2, bHeight//2:], threshold)
         splitAndMerge(bL, blank[bWidth//2:, :bHeight//2], threshold)
         splitAndMerge(bR, blank[bWidth//2:, bHeight//2:], threshold)
+        '''This section controls the merging of all regions within a quadrant'''
         if flatTest(np.concatenate([tL, tR], axis=1), threshold):
             if flatTest(np.concatenate([tL, bL], axis=0), threshold):
                 blank[:bWidth//2, :bHeight//2] = np.full(tL.shape, np.mean(np.concatenate([tL, tR], axis=1)))
@@ -71,9 +67,14 @@ def splitAndMerge(img, blank, threshold):
 
 
 def flatTest(array, threshold):
+    '''Takes an np.array and a threshold, and outputs TRUE if the contents of the array have a
+        statistical variance lower than the given threshold, and false if it does not
+
+        This is the P predicate described in the paper; its function is to serve as the basis for whether
+        or not to split, and whether or not to merge, a given region or two regions'''
     x, y = array.shape
     mean = []
-    threshold = threshold #/ ((x * y) / 2)
+    threshold = threshold
     for j in range(x):
         for k in range(y):
             mean.append(array[j,k])
@@ -81,26 +82,7 @@ def flatTest(array, threshold):
         return True
     else:
         return False
-
-def eraseEdge(edges, bounds, axis=0):
-    x,y = edges.shape
-    i, j = bounds[0]
-    if axis == 1:
-        while (i, j) != bounds[1]:
-            print(edges[i][j])
-            edges[i][j] = 0
-            if i-1 < x:
-                edges[i-1][j] = 0
-            j += 1
-    if axis == 0:
-        while (i, j) != bounds[1]:
-            print(edges[i][j])
-            edges[i][j] = 0
-            if j-1 < y:
-                edges[i][j-1] = 0
-            i += 1
-    return edges
-
+        
 if __name__ == '__main__':
     img = Image.open("22093.jpg")
     array = regionSplitAndMerge(img, 450)
